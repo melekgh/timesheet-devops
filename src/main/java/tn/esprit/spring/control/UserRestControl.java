78e40eaef19055da211fe4494212ccf2cc108da3
mghrab <mghrab@vermeg.com>
2024-04-20 11:23:22 +0100

    changement11

--- src/main/java/tn/esprit/spring/control/UserRestControl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControl.java
@@ -17,7 +17,7 @@ public class UserRestControl {
 	@Autowired 
 	IUserService userService; 
 
-	// hello melek
+	// hello melekkk
 	// URL : http://localhost:????/????/????/retrieve-all-users
 	@GetMapping("/retrieve-all-users")
 	public List<User> retrieveAllUsers() {
